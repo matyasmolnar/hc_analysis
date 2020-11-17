be8d9fae14466810af232c7d123f818b9b6a436c
Matyas Molnar <mdm49@cam.ac.uk>
2020-11-17 11:46:41 +0000

    imshow plot of visibilities moved out of nb

--- cal_utils.py
+++ cal_utils.py
@@ -7,7 +7,7 @@ from matplotlib import pyplot as plt
 
 def waterfall_column(waterfalls, flags, titles, ylabel, clims=None, clabels=None, cmaps=None, \
                      ylims=None, extents=None, hspace=.1, figsize=(12,6), dpi=100):
-    '''Useful plotting function for the IDR 2.2 memo.'''
+    """Useful plotting function for the IDR2.2 memorandum"""
     if clims is None:
         clims = [None for i in range(len(waterfalls))]
     if clabels is None:
@@ -33,4 +33,25 @@ def waterfall_column(waterfalls, flags, titles, ylabel, clims=None, clabels=None
         ax.set_ylim(ylim)
         props = dict(boxstyle='round', facecolor='white', alpha=0.8)
         ax.text(0.02, 0.9, t, transform=ax.transAxes, fontsize=14, verticalalignment='top', bbox=props)
+    plt.show()
+
+
+def plot_vis(data, flags, hd, JD, bl, vcomp, title=None, vmax=.1, figsize=(12,3), dpi=100):
+    if vcomp == 'amp':
+        label = 'Amplitude'
+        vcalc = np.absolute
+    elif vcomp == 'phase':
+        label = 'Phase (Radians)'
+        vcalc = np.angle
+        vmax = None
+    else:
+        raise ValueError('Specify either {"amp", "phase"} for vcomp')
+    plt.figure(figsize=figsize, dpi=dpi)
+    plt.imshow(vcalc(data[bl]) / (~flags[bl]), aspect='auto', cmap='twilight',
+               extent=[hd.freqs[0]/1e6, hd.freqs[-1]/1e6, hd.times[-1]-int(JD), hd.times[0]-int(JD)], \
+               vmax=vmax)
+    plt.ylabel('JD - {}'.format(int(JD)))
+    plt.xlabel('Frequency (MHz)')
+    plt.title(title + ': ' + str(bl))
+    plt.colorbar(label=label, aspect=8, pad=.025)
     plt.show()
